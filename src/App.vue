0602c4e882fe22fe96c1fa85aa386b8676810497
Paul Hoover <polyphonic13@gmail.com>
2020-05-31 23:04:29 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+    <div id="app">
+        <router-view />
+    </div>
+</template>
+
+<script src="./App.js"></script>
+<style src="./App.css"></style>
---
+++ src/components/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="home">
+        <img
+            class="large-logo"
+            src="../../assets/images/polyworks-logo-gradient-dark.png"
+        >
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+    data() {
+        return {};
+    }
+};
+</script>
+<style scoped src="./Home.css"></style>
