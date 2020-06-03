9f2fb5b07cbc5806d218cdb20c172ff3e19117f9
Paul Hoover <polyphonic13@gmail.com>
2020-06-03 09:14:28 -0400

    added about & connect components, lato fonts

---
+++ src/components/about/About.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="games screen">
+        <h1 class="brandon text-xl">A B O U T</h1>
+        <p class="text-xl text-left text-white">We build games. From simple to complex, each game is made with the goal of making a creative experience for the player. Our games are intended to evoke feelings, inspire, immerse in alternate universes or just distract. The world has enough conflict as it is.</p>
+    </div>
+</template>
+<script>
+export default {
+    name: "About"
+};
+</script>
+<style scoped>
+</style>
---
+++ src/components/connect/Connect.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="connect screen">
+        <h1 class="brandon text-xl">C O N N E C T</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: "Connect"
+};
+</script>
+<style scoped>
+</style>
