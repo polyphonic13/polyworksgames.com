9a0b5b298aea40e40b1e79266dc73883f5e32a97
Paul Hoover <polyphonic13@gmail.com>
2020-06-09 17:06:43 -0400

    more style tweaks

--- src/App.vue
+++ src/App.vue
@@ -35,6 +35,7 @@ body {
     width: 100%;
     height: 100%;
     padding: 0;
+    margin: 0;
 }
 
 p {
@@ -80,8 +81,6 @@ a {
 
     overflow-y: auto;
     overflow-x: hidden;
-
-    margin: 2em;
 }
 
 @font-face {
