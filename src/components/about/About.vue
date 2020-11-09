51dcc7cc85c0c783df6e82476112d0a8f8c37ccd
Paul Hoover <polyphonic13@gmail.com>
2020-11-09 09:15:19 -0500

    About page updated

--- src/components/about/About.vue
+++ src/components/about/About.vue
@@ -1,19 +1,16 @@
 <template>
     <div class="games screen">
         <h1 class="brandon text-xl">A B O U T</h1>
-        <!-- <p class="text-lg text-left text-white">We build games. From simple to complex, each game is made with the goal of making a creative experience for the player. Our games are intended to evoke feelings, inspire, immerse in alternate universes or just distract. The world has enough conflict.</p> -->
         <section>
-            <p class="text-lg text-left">Games. Mobile Games. Desktop Games. HTML5 Games. Console and VR. Games to make you think. Explore. Search. Discover. Solve Mysteries. Solve Puzzles. Experience other worlds. Defend other worlds. Make friends. Defeat enemies. Laugh. Cry. Feel. Think. Triumph. Win.</p>
+            <p class="text-lg text-left">We build games. Mobile Games. Desktop Games. HTML5 Games. Console and VR. Games to make you think. Explore. Search. Discover. Solve Mysteries. Solve Puzzles. Experience other worlds. Defend other worlds. Make friends. Defeat enemies. Laugh. Cry. Feel. Think. Triumph. Win.</p>
             <br />
-            <p class="text-lg">This is what Polyworks Games creates.</p>
-            <br />
-            <p class="text-lg text-left">A portion of all proceeds is given back to charities. We feel strongly about game play being both enjoyable and having real-world impact. See individual game pages for more infomation.</p>
+            <p class="text-lg text-left">A portion of all proceeds is given back to charities. We feel strongly about game play being both enjoyable and having real-world impact. See individual game pages for more information.</p>
         </section>
     </div>
 </template>
 <script>
 export default {
-    name: "About"
+    name: "About",
 };
 </script>
 <style scoped>
