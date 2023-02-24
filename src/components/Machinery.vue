e7868732ff72a8774bf5675c56b547ffa2af5c59
Paul Hoover <polyphonic13@gmail.com>
2023-02-24 12:13:11 -0500

    onclick css animations added to machinery items

--- src/components/Machinery.vue
+++ src/components/Machinery.vue
@@ -1,17 +1,22 @@
 <template>
-    <div class="machinery-container">
+    <div
+        class="machinery-container"
+        :class="(isVisible) ? 'visible' : 'hidden'"
+    >
         <div
             v-if="isRendered"
             class="machinery"
-            :class="(isVisible) ? 'visible' : 'hidden'"
         >
             <div
                 v-for="i in machineryItemCount"
                 :key="i"
+                class="machinery-item"
+                @click="onItemClicked($event)"
                 style="top:10px;left:20px;"
                 :style="{
-                        top: `${(Math.random() * 192) - 64}px`,
+                        top: `${((i % 8) * 16) - Math.floor(Math.random() * (64 - 16) + 16)}px`,
                         left: `${Math.random() * elWidth}px`,
+                        scale: `${((i % 8) * 0.1) + 0.5}`,
                         transform: `rotate(${getRandomRotationStyle()}deg)` }"
             ></div>
         </div>
@@ -44,6 +49,14 @@ export default {
         getRandomRotationStyle() {
             return Math.floor(Math.random() * 350);
         },
+        onItemClicked(evt) {
+            const target = evt.currentTarget;
+            if (Array.from(target.classList).includes("spin-animation")) {
+                target.classList.remove("spin-animation");
+                return;
+            }
+            target.classList.add("spin-animation");
+        },
     },
 };
 </script>
@@ -55,7 +68,7 @@ export default {
         rgba(171, 205, 239, 0.5)
     );
     width: 100%;
-    height: 128px;
+    height: 256px;
     bottom: 0;
 
     position: fixed;
@@ -67,10 +80,43 @@ export default {
     position: relative;
 }
 
-.machinery > * {
+.machinery-item {
     position: absolute;
-    border: 1px rgba(255, 255, 255, 0.33) solid;
-    width: 32px;
-    height: 32px;
+    background-color: rgba(255, 255, 255, 0);
+    border: 1px rgba(255, 255, 255, 0.2) solid;
+    width: 64px;
+    height: 64px;
+
+    transition: all 1s linear;
+}
+
+.machinery-item:hover {
+    background-color: rgba(255, 255, 255, 1);
+    border: 1px rgba(171, 205, 239, 0.9) solid;
+    cursor: pointer;
+}
+
+.spin-animation {
+    -webkit-animation: spin 4s linear infinite;
+    -moz-animation: spin 4s linear infinite;
+    animation: spin 4s linear infinite;
+}
+
+@-moz-keyframes spin {
+    100% {
+        -moz-transform: rotate(360deg);
+    }
+}
+@-webkit-keyframes spin {
+    100% {
+        -webkit-transform: rotate(360deg);
+    }
+}
+
+@keyframes spin {
+    100% {
+        -webkit-transform: rotate(360deg);
+        transform: rotate(360deg);
+    }
 }
 </style>
